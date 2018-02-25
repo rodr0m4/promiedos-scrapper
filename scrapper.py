45d46c4b0159948e8fa3dbc7744220158202ce62
Rodrigo Martin <rodrigoleonardomartin@gmail.com>
2018-02-25 20:48:42 -0300

    Right now it just prints match_data to stdout (because docker). output.json has a sample of the data once beautified.

--- scrapper.py
+++ scrapper.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python 
-
+from re import compile
 from sys import argv
 from apscheduler.schedulers.background import BackgroundScheduler
 from custom_types import get_match, Card
 from bs4 import BeautifulSoup
+from json import dumps
 import requests
+
+
+def translate(string):
+    translations = {
+        'goles': 'goals',
+        'amarillas': 'yellow_cards',
+        'rojas': 'red_cards',
+        'cambios': 'subs'
+    }
+    return translations[string]
 
 
 def formacion(soup, home_or_away):
@@ -27,15 +38,64 @@ def incidencia(formacion, incidencia_type):
     ['amarillas', 'cambios', 'rojas', 'goles'], it will return a string contained in a \
     td of class 'incidencias2'
     """
-    # If its a 'gol' the previous tr has a td of class .incidencias1, otherwise it has \
+    # If its a 'gol' the previous tr has a td of class .incidencias1, otherwise it has
     # the same class as the parameter incidencia.
     td_class = 'incidencias1' if incidencia_type is 'goles' else incidencia_type
-    prev_sibling_tr = formacion.find('td', attrs={ 'class': td_class }).parent
-    return prev_sibling_tr.next_sibling.find('td', attrs={ 'class': 'incidencias2' })
+    prev_sibling_td = formacion.find('td', attrs={ 'class': td_class })
+    if prev_sibling_td:
+        prev_sibling_tr = prev_sibling_td.parent
+        return prev_sibling_tr.next_sibling.find('td', attrs={ 'class': 'incidencias2' })
+
+
+def parse_minute_and_player(string):
+    """
+    Given a string like '30\' Some. Player' it deconstructs it into minutes and player
+    """
+    minute_and_player = dict()
+    for subs in string.split('\''):
+        try:
+            minute_and_player['minute'] = int(subs)
+        except ValueError:
+            minute_and_player['player'] = subs[1:]
+    return minute_and_player
+
+
+
+def parse_incidencia(incidencia):
+    if incidencia:
+        string = incidencia.string
+        if '⇆' in string:
+            return 'kappa'
+        else:
+            events = []
+            for s in string.split(';'):
+                if s is not ' ' or '':
+                    events.append(
+                        parse_minute_and_player(s[1:] if s.startswith(' ') else s)
+                    )
+            # return [s for s in string.split(';') if s is not ' ' and s is not '62\' L. Fernandez']
+        return events
+            
+        
 
 
 def get_changes(url, match_data):
-    return 0
+    document = requests.get(url).content
+    soup = BeautifulSoup(document, 'lxml')
+    
+    match_data = dict()
+    match_data['match_id'] = url
+    for x in range(1, 3):
+        a_formacion = formacion(soup, x)
+        team = dict()
+        team['name'] = team_name(a_formacion)
+        for i in ['goles', 'amarillas', 'rojas']:
+            incidencias = parse_incidencia(incidencia(a_formacion, i))
+            team[translate(i)] = incidencias if incidencias else []
+        match_data[
+            'home_team' if x is 1 else 'away_team'
+        ] = team
+    print(dumps(match_data))
 
 
 def main():
@@ -53,7 +113,7 @@ def main():
     match_data = get_match()
 
     get_changes(url, match_data)
-    return 1
+    return 0
 
 
 if __name__ == '__main__':
